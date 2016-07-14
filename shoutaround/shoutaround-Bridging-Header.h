27d7cb1ae35f02aa3f04f1ac1c6b1d9794c9ed90
planert41 <planert41@gmail.com>
2016-07-14 16:03:02 -0500

    Added Login Screen

--- shoutaround/shoutaround-Bridging-Header.h
+++ shoutaround/shoutaround-Bridging-Header.h
@@ -6,7 +6,6 @@
 #import <ParseUI/ParseUI.h>
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
 #import <ParseTwitterUtils/ParseTwitterUtils.h>
-#import <ParseUI/ParseUI.h>
 #import <Bolts/Bolts.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
